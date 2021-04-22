46cd53bdf8c72b1895206ecfed4e12093586531d
Maruko <38086766+zlMaku@users.noreply.github.com>
2021-04-22 11:56:46 -0500

    Docs agregado

---
+++ docs/precache-manifest.4a932e5f2b7a82338ec03ad711748732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f1d021aeea6367aa532e3b189998e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5117c42f0e1ef03d257",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5117c42f0e1ef03d257",
+    "url": "/static/js/main.8c53dce0.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
